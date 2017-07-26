c2ec98d1256b42d0a3ca6379a1eb69ac77358f5d
Jay Clarens <Clarens.Jay@gmail.com>
2017-07-26 14:46:36 +0700

    add location seed

--- database/seeds/UserContactTableSeeder.php
+++ database/seeds/UserContactTableSeeder.php
@@ -17,56 +17,56 @@ class UserContactTableSeeder extends Seeder
                 'phone' => '081234567890',
                 'city' => 1,
                 'address' => 'Jl. Besar',
-                'location' => ''
+                'location' => '3.610485, 98.675769'
             ],
             [
                 'user_id' => 2,
                 'phone' => '081234123412',
                 'city' => 2,
                 'address' => 'Jl. Satu',
-                'location' => ''
+                'location' => '3.595237, 98.652423'
             ],
             [
                 'user_id' => 3,
                 'phone' => '0888888888',
-                'city' => 4,
+                'city' => 3,
                 'address' => 'Jl. Sana',
-                'location' => ''
+                'location' => '3.605688, 98.707355'
             ],
             [
                 'user_id' => 4,
                 'phone' => '0888888888',
-                'city' => 3,
+                'city' => 4,
                 'address' => 'Jl. Sini',
-                'location' => ''
+                'location' => '3.577248, 98.653796'
             ],
             [
                 'user_id' => 5,
                 'phone' => '081234432165',
                 'city' => 5,
                 'address' => 'Jl. Riau',
-                'location' => ''
+                'location' => '3.587870, 98.702205'
             ],
             [
                 'user_id' => 6,
                 'phone' => '080808080808',
-                'city' => 1,
+                'city' => 6,
                 'address' => 'Jl. Padang',
-                'location' => ''
+                'location' => '3.588384, 98.681434'
             ],
             [
                 'user_id' => 7,
                 'phone' => '08080881123',
-                'city' => 2,
+                'city' => 7,
                 'address' => 'Jl. Satu Lagi',
-                'location' => ''
+                'location' => '3.572794, 98.656886'
             ],
             [
                 'user_id' => 8,
                 'phone' => '08080881125',
                 'city' => 8,
                 'address' => 'Jl. Itu',
-                'location' => ''
+                'location' => '3.569196, 98.701175'
             ]
         ]);
     }
