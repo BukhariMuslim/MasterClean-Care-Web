2d42977a713e0af190cfabd668d0fdcc62d478b9
BukhariMuslim <bm.bukharimuslim@gmail.com>
2017-07-26 14:36:06 +0700

    Fix User contact seeder places id

--- database/seeds/UserContactTableSeeder.php
+++ database/seeds/UserContactTableSeeder.php
@@ -15,49 +15,49 @@ class UserContactTableSeeder extends Seeder
             [
                 'user_id' => 1,
                 'phone' => '081234567890',
-                'city' => 5,
+                'city' => 1,
                 'address' => 'Jl. Besar',
                 'location' => ''
             ],
             [
                 'user_id' => 2,
                 'phone' => '081234123412',
-                'city' => 7,
+                'city' => 2,
                 'address' => 'Jl. Satu',
                 'location' => ''
             ],
             [
                 'user_id' => 3,
                 'phone' => '0888888888',
-                'city' => 6,
+                'city' => 4,
                 'address' => 'Jl. Sana',
                 'location' => ''
             ],
             [
                 'user_id' => 4,
                 'phone' => '0888888888',
-                'city' => 6,
+                'city' => 3,
                 'address' => 'Jl. Sini',
                 'location' => ''
             ],
             [
                 'user_id' => 5,
                 'phone' => '081234432165',
-                'city' => 13,
+                'city' => 5,
                 'address' => 'Jl. Riau',
                 'location' => ''
             ],
             [
                 'user_id' => 6,
                 'phone' => '080808080808',
-                'city' => 12,
+                'city' => 1,
                 'address' => 'Jl. Padang',
                 'location' => ''
             ],
             [
                 'user_id' => 7,
                 'phone' => '08080881123',
-                'city' => 11,
+                'city' => 2,
                 'address' => 'Jl. Satu Lagi',
                 'location' => ''
             ],
