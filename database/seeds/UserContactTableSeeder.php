653606eae9917ea7c7389062ed3114d18cc564d2
Jay Clarens <Clarens.Jay@gmail.com>
2017-08-08 16:30:37 +0700

    edit seed contact city to medan

--- database/seeds/UserContactTableSeeder.php
+++ database/seeds/UserContactTableSeeder.php
@@ -29,63 +29,63 @@ class UserContactTableSeeder extends Seeder
             [
                 'user_id' => 3,
                 'phone' => '0888888888',
-                'city' => 3,
+                'city' => 2,
                 'address' => 'Jl. Sana',
                 'location' => '3.605688, 98.707355'
             ],
             [
                 'user_id' => 4,
                 'phone' => '0888888888',
-                'city' => 4,
+                'city' => 2,
                 'address' => 'Jl. Sini',
                 'location' => '3.577248, 98.653796'
             ],
             [
                 'user_id' => 5,
                 'phone' => '081234432165',
-                'city' => 5,
+                'city' => 2,
                 'address' => 'Jl. Riau',
                 'location' => '3.587870, 98.702205'
             ],
             [
                 'user_id' => 6,
                 'phone' => '080808080808',
-                'city' => 6,
+                'city' => 2,
                 'address' => 'Jl. Padang',
                 'location' => '3.588384, 98.681434'
             ],
             [
                 'user_id' => 7,
                 'phone' => '08080881123',
-                'city' => 7,
+                'city' => 2,
                 'address' => 'Jl. Satu Lagi',
                 'location' => '3.572794, 98.656886'
             ],
             [
                 'user_id' => 8,
                 'phone' => '08080198897',
-                'city' => 8,
+                'city' => 2,
                 'address' => 'Jl. Itu',
                 'location' => '3.569196, 98.701175'
             ],
             [
                 'user_id' => 9,
                 'phone' => '08086784563',
-                'city' => 8,
+                'city' => 2,
                 'address' => 'Jl. Jepara',
                 'location' => '3.569196, 98.701175'
             ],
             [
                 'user_id' => 10,
                 'phone' => '08086283749',
-                'city' => 5,
+                'city' => 2,
                 'address' => 'Jl. Thamrin',
                 'location' => '3.569196, 98.701175'
             ],
             [
                 'user_id' => 11,
                 'phone' => '08080739286',
-                'city' => 6,
+                'city' => 2,
                 'address' => 'Jl. Denai',
                 'location' => '-6.154048, 106.776076'
             ],
@@ -99,42 +99,42 @@ class UserContactTableSeeder extends Seeder
             [
                 'user_id' => 13,
                 'phone' => '08082918375',
-                'city' => 1,
+                'city' => 2,
                 'address' => 'Jl. Mangga',
                 'location' => '3.590341, 98.679223'
             ],
             [
                 'user_id' => 14,
                 'phone' => '08089572837',
-                'city' => 4,
+                'city' => 2,
                 'address' => 'Jl. Padi',
                 'location' => '3.615525, 98.651586'
             ],
             [
                 'user_id' => 15,
                 'phone' => '08080925835',
-                'city' => 3,
+                'city' => 2,
                 'address' => 'Jl. Buntu',
                 'location' => '3.577834, 98.666349'
             ],
             [
                 'user_id' => 16,
                 'phone' => '08080886392',
-                'city' => 6,
+                'city' => 2,
                 'address' => 'Jl. Ujung',
                 'location' => '3.566698, 98.701024'
             ],
             [
                 'user_id' => 17,
                 'phone' => '08089882125',
-                'city' => 8,
+                'city' => 2,
                 'address' => 'Jl. Merdeka',
                 'location' => '3.550764, 98.651071'
             ],
             [
                 'user_id' => 18,
                 'phone' => '08080881125',
-                'city' => 7,
+                'city' => 2,
                 'address' => 'Jl. Halat',
                 'location' => '3.566698, 98.712869'
             ]
