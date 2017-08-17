1e38af5155930bcaec2a15ee5e884b11c2bda3b9
BukhariMuslim <bm.bukharimuslim@gmail.com>
2017-08-17 21:18:26 +0700

    Fix Admin Seeder

--- database/seeds/PermissionRoleTableSeeder.php
+++ database/seeds/PermissionRoleTableSeeder.php
@@ -15,8 +15,19 @@ class PermissionRoleTableSeeder extends Seeder
     {
         $role = Role::where('name', 'admin')->firstOrFail();
 
-        $permissions = Permission::all();
-
+        $permissions = Permission::whereNotIn('key',
+                        [
+                            'add_critisms',
+                            'edit_critisms',
+                            'add_reports',
+                            'edit_reports',
+                            'add_emergency_calls',
+                            'edit_emergency_calls',
+                            'add_review_orders',
+                            'edit_review_orders',
+                            'add_wallet_transactions',
+                        ]);
+        
         $role->permissions()->sync(
             $permissions->pluck('id')->all()
         );
