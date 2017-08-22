f7d07037c3bb51023ad596d2be7f22f61cf4cfbb
Jay Clarens <Clarens.Jay@gmail.com>
2017-08-22 13:39:47 +0700

    fiiiix lg

--- database/seeds/WalletTransactionsTableSeeder.php
+++ database/seeds/WalletTransactionsTableSeeder.php
@@ -17,7 +17,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>2,
                 'amount'=>5000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '06', '19', '10', '15', '0'),
                 'status'=> 1,
 		'created_at' = > Carbon::create('2017', '06', '19', '10', '15', '0')
@@ -26,7 +26,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>2,
                 'amount'=>5000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '07', '21', '10', '15', '0'),
                 'status'=> 1,
 		'created_at' = > Carbon::create('2017', '07', '21', '10', '15', '0')
@@ -35,7 +35,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>2,
                 'amount'=>1000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '07', '24', '10', '15', '0'),
                 'status'=> 0,
 		'created_at' = > Carbon::create('2017', '07', '24', '10', '15', '0')
@@ -44,7 +44,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>3,
                 'amount'=>5000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '07', '19', '1', '16', '0'),
                 'status'=> 0,
 		'created_at' = > Carbon::create('2017', '07', '19', '1', '16', '0')
@@ -53,7 +53,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>4,
                 'amount'=>1000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '07', '19', '8', '10', '0'),
                 'status'=> 0,
 		'created_at' = > Carbon::create('2017', '07', '19', '8', '10', '0')
@@ -62,7 +62,7 @@ class WalletTransactionsTableSeeder extends Seeder
                 'user_id'=>5,
                 'amount'=>1000000,
                 'trc_type'=>0,
-		'trc_img'=>'ncBa7ORi81VueI9Sv3Aw.jpg',
+		'trc_img'=>'users/ncBa7ORi81VueI9Sv3Aw.jpg',
                 'trc_time'=> Carbon::create('2017', '07', '19', '11', '9', '0'),
                 'status'=> 0,
 		'created_at' = > Carbon::create('2017', '07', '19', '11', '9', '0')
