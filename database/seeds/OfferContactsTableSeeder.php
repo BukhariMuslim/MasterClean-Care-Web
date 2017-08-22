4749073827dd60c9912c3fa20fbd65ee76fff3b5
Jay Clarens <Clarens.Jay@gmail.com>
2017-08-22 14:24:58 +0700

    again

--- database/seeds/OfferContactsTableSeeder.php
+++ database/seeds/OfferContactsTableSeeder.php
@@ -14,23 +14,51 @@ class OfferContactsTableSeeder extends Seeder
         DB::table('offer_contacts')->insert([
             [
                 'offer_id'=>1,
-                'phone'=>'081234432165',
-                'city'=>2,
-                'address' => 'Jl. Riau',
+                'phone' => '081234432165',
+                'city' => 2,
+                'address' => 'Jl. Keluarga No.22, Denai, Medan',
                 'location' => '3.587870, 98.702205'
             ],
             [
                 'offer_id'=>2,
-                'phone'=>'081234123412',
+                'phone' => '082168360303',
                 'city' => 2,
-                'address' => 'Jl. Satu',
+                'address' => 'Jl. Gotong Royong No.22, Denai, Medan',
                 'location' => '3.595237, 98.652423'
-            ],    
+            ],
             [
                 'offer_id'=>3,
-                'phone'=>'081234123412',
+                'phone' => '082168360303',
                 'city' => 2,
-                'address' => 'Jl. Satu',
+                'address' => 'Jl. Gotong Royong No.22, Denai, Medan',
+                'location' => '3.595237, 98.652423'
+            ],
+            [
+                'offer_id'=>4,
+                'phone' => '0888888888',
+                'city' => 2,
+                'address' => 'Jl. Gotong Royong No.22, Denai, Medan',
+                'location' => '3.605688, 98.707355',
+            ],
+            [
+                'offer_id'=>4,
+                'phone' => '0888888888',
+                'city' => 2,
+                'address' => 'Jl. Bersama No.2, Denai, Medan',
+                'location' => '3.577248, 98.653796',
+            ],
+            [
+                'offer_id'=>4,
+                'phone' => '081234432165',
+                'city' => 2,
+                'address' => 'Jl. Keluarga No.22, Denai, Medan',
+                'location' => '3.587870, 98.702205',
+            ],
+            [
+                'offer_id'=>4,
+                'phone' => '082168360303',
+                'city' => 2,
+                'address' => 'Jl. Gotong Royong No.22, Denai, Medan',
                 'location' => '3.595237, 98.652423'
             ],
         ]);
