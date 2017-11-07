a7d13b5001b9c4f271c70258363f88e2c7a91be2
Bukhari Muslim <BukhariMuslim@users.noreply.github.com>
2017-11-07 16:39:27 +0700

    Update APP_TIMEZONE

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Jakarta'),
 
     /*
     |--------------------------------------------------------------------------
